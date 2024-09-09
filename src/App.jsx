8712aa253d3417bd15a1703ffece90e4f98f22f8
leen7awa <leen.hawa@e.braude.ac.il>
2024-09-09 13:55:30 +0300

    height fix

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ return (
   <>
     <Header />
     <Router>
-      <div className="h-screen sm:h-auto md:h-screen bg-cover bg-center flex justify-center items-center w-full sm:w-auto md:w-full bg-slate-600">
+      <div className="h-screen sm:h-auto md:h-screen bg-cover bg-center flex justify-center items-center w-full sm:w-auto md:w-full bg-slate-600 pb-40">
         <Routes>
           <Route path="/" element={<SeatingPage onSelectOption={handleSelectOption} />} />
           <Route path="/menu" element={<MenuPage addItemToCart={addItemToCart} cartItems={cartItems} />} />
